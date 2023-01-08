79785b36da2cbb9b4a227ae4219d803ecd9a6983
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2023-01-07 22:34:10 -0800

    Update code.java

--- Day16/code.java
+++ Day16/code.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 class UsernameValidator {
      public static final String regularExpression = "^[A-Za-z][A-Za-z0-9_]{7,29}$";
 }
-
-
 public class Solution {
     private static final Scanner scan = new Scanner(System.in);
     
