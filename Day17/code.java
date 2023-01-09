e8fbb5118c92f91f8126e3608fca289a42bb1e5c
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2023-01-09 13:16:07 +0530

    Create code.java

---
+++ Day17/code.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+static Scanner sc = new Scanner(System.in);
+    static boolean flag;
+    static int B = sc.nextInt();
+    static int H = sc.nextInt();
+    static{
+        if(B>0 && H>0)
+        {
+            flag=true;
+        }
+        else
+        {
+            flag=false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
