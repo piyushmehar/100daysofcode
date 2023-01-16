cc6f26716b4e9f1bf132f4106715bf7d23ab287f
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2023-01-16 12:20:47 +0530

    Create code.py

---
+++ Day24/code.py
@@ -0,0 +1,12 @@
+def swap_case(s):
+    
+    return s.swapcase()
+
+if __name__ == '__main__':
+  
+  
+  
+#   program to swap the case of  the given str.
+#   eg:
+#   Www.HackerRank.com → wWW.hACKERrANK.COM
+#   Pythonist 2 → pYTHONIST 2  
