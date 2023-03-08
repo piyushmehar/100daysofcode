568f9cdff58bbeb84bd94699b7b2fbb35e8dcc2b
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2023-03-08 14:43:18 +0530

    Create code.java

---
+++ Day75/code.java
@@ -0,0 +1,35 @@
+// 875. Koko Eating Bananas
+
+// Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+// Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low=1;
+        int high=Integer.MIN_VALUE;
+        for(int i=0;i<piles.length;i++){
+            high=Math.max(high,piles[i]);
+        }
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(blackbox(mid,piles,h)){
+                high=mid;
+            }
+            else
+                low=mid+1;
+        }
+        return low;
+    }
+    public boolean blackbox(int maxpiles,int[] piles,int h){
+        int hours=0;
+        for(int i:piles){
+            int time=i/maxpiles;
+            hours+=time;
+            if(i%maxpiles!=0) hours++;
+        }
+        if(hours<=h)
+            return true;
+        return false;
+    }
+}
