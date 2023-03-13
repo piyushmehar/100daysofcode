30caadc1f3c9214ca57b4d07600f834b45ce5aa3
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2023-03-13 16:59:22 +0530

    Create code.java

---
+++ Day80/code.java
@@ -0,0 +1,19 @@
+// 101. Symmetric Tree
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        return t1.val == t2.val && isMirror(t1.right, t2.left) && isMirror(t1.left, t2.right);
+    }
+}
