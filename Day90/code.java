561d5b32dd1d45df63cc46461b84dc0234b6b83e
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2023-03-23 10:08:41 +0530

    Create code.java

---
+++ Day90/code.java
@@ -0,0 +1,29 @@
+// 1319. Number of Operations to Make Network Connected
+// There are n computers numbered from 0 to n - 1 connected by ethernet cables connections forming a network where connections[i] = [ai, bi] represents a connection between computers ai and bi. Any computer can reach any other computer directly or indirectly through the network.
+// You are given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected.
+// Return the minimum number of times you need to do this in order to make all the computers connected. If it is not possible, return -1.
+
+
+class Solution {
+    int dfs(int u, List<Integer>[] graph, boolean[] visited) {
+        if (visited[u]) return 0;
+        visited[u] = true;
+        for (int v : graph[u]) dfs(v, graph, visited);
+        return 1;
+    }
+
+    public int makeConnected(int n, int[][] connections) {
+        if (connections.length < n - 1) return -1;
+        List<Integer>[] graph = new List[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int[] c : connections) {
+            graph[c[0]].add(c[1]);
+            graph[c[1]].add(c[0]);
+        }
+        
+        int components = 0;
+        boolean[] visited = new boolean[n];
+        for (int v = 0; v < n; v++) components += dfs(v, graph, visited);
+        return components - 1;
+    }
+}
