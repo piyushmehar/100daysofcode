05e33d403964f334d34272dad56b1c5fe74cb3e1
PIYUSH MEHAR <80638299+piyushmehar@users.noreply.github.com>
2023-11-16 23:04:52 +0530

    Create 1980. Find Unique Binary String.java

---
+++ 1980. Find Unique Binary String.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        Set<String> set = new HashSet<>();
+
+        for (String num : nums) {
+            set.add(num);
+        }
+
+        int n = nums.length;
+        for (int i = 0; i < (1 << n); i++) {
+            String binary = Integer.toBinaryString(i);
+            while (binary.length() < n) {
+                binary = "0" + binary;
+            }
+
+            if (!set.contains(binary)) {
+                return binary;
+            }
+        }
+
+        return "";
+    }
+}
